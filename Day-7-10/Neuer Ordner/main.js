62d1b802d8d294baff7e1ade1c417119fcb63170
Alex <kusznir.andrzej@gmail.com>
2019-10-07 10:36:33 +0200

    first commit

---
+++ Day-7-10/Neuer Ordner/main.js
@@ -0,0 +1,13 @@
+var temperatur = Math.random()*(25-(-5))+(-5);
+
+if (-5 <=temperatur<=10 ) {
+
+	 document.getElementById("result").innerHTML = "The weather is cold";
+
+
+} else{
+
+	 document.getElementById("result").innerHTML = "The weather is moderate";
+
+
+}
